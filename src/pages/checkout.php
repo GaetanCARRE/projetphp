f131bcb6b81cf47a90ec905387f0ba6cc1cdae8c
GaetanCARRE <gaetancarre.pro@gmail.com>
2022-06-17 16:15:32 +0200

    formulaire paiement

--- src/pages/checkout.php
+++ src/pages/checkout.php
@@ -8,23 +8,44 @@
         <div class="delete-checkout"></div>
         <!-- A afficher en meme temps que produit taille quantitée prix  -->
         <a href="">
-                <ion-icon name="close-circle-outline"></ion-icon>
+            <ion-icon name="close-circle-outline"></ion-icon>
         </a>
         <!-- A afficher en meme temps que produit taille quantitée prix  -->
 
     </div>
-    <div class="payment-checkout">
-        <h2>Information de Paiement</h2>
-        <div>Méthode de paiement
-            <br>
-            <input type="checkbox" class="payment-checkbox"> <label>Visa</label><br>
-            <input type="checkbox" class="payment-checkbox"> <label>MasterCard</label>
 
-        </div>
-        <div>Nom sur la carte</div>
-        <div>Numéro de Carte</div>
-        <div>Date d'expiration</div>
-       
-        
+    <div class="payment-checkout">
+            <form action="" method="post">
+                <h2>Paiement</h2>
+                <div>Méthode de paiement
+                    <br>
+                    <input type="checkbox" class="payment-checkbox"> <label>Visa</label><br>
+                    <input type="checkbox" class="payment-checkbox"> <label>MasterCard</label>
+
+                </div>
+
+                Nom sur la carte
+                <input type="card_name" name="card_name" placeholder="Prenom Nom" required />
+                Numero de carte
+                <input type="card_num" name="card num" placeholder="Numero de la carte" required />
+                Date d'expiration
+                <input id="expiration_date" name="expiration_date" placeholder="MM/YY" required />
+                CVV
+                <input id="cvv" name="cvv" placeholder="cvv" required />
+
+
+                <h2>Livraison</h2>
+
+
+                Adresse
+                <input type="adresse" name="adresse" required />
+                Ville
+                <input type="Ville" name="Ville" placeholder="" required />
+                Code Postal
+                <input type="CodePostal" name="CodePostal" placeholder="" required />
+
+                <button class='button-payment' type="submit" className="button">Payer</button>
+            </form>
     </div>
+
 </div>
